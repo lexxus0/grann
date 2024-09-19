6bcdce731b04a75f07bac6d713657ab6682f24a4
Serhii Svitlychnyi <svetlichniy1905@gmail.com>
2024-09-19 17:28:10 +0300

    Додав до секції бестселерс десктопну версію, та почав створювати db.json

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -1,31 +1,60 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.bestsellers__slider.swiper', {
+const swiperMobile = new Swiper('.bestsellers__slider_mobile.swiper', {
   direction: 'horizontal',
   speed: 400,
   spaceBetween: 100,
   loop: true,
 
   pagination: {
-    el: '.swiper-pagination.bestsellers__slider-pagination',
+    el: '.swiper-pagination.bestsellers__slider_mobile-pagination',
     clickable: true,
   },
+
+  navigation: {
+    nextEl: '.swiper-button-next.bestsellers__slider_mobile-btn_next',
+    prevEl: '.swiper-button-prev.bestsellers__slider_mobile-btn_prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const swiperDesktop = new Swiper('.bestsellers__slider.swiper', {
+  direction: 'horizontal',
+  speed: 400,
+  loop: true,
+
   navigation: {
     nextEl: '.swiper-button-next.bestsellers__slider-btn_next',
     prevEl: '.swiper-button-prev.bestsellers__slider-btn_prev',
   },
+
   breakpoints: {
     0: {
       slidesPerView: 1,
     },
     768: {
       slidesPerView: 2,
-    }
-  }
+      spaceBetween: 50,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
 });
-
-swiper();
