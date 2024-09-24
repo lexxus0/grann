0002546769f8c482aae850ebf5dad4412040c50d
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-09-25 01:53:12 +0200

    work with swiper

---
+++ src/js/instagram.js
@@ -0,0 +1,19 @@
+import Swiper from "swiper/bundle";
+
+const swiperInsta = new Swiper('.insta-swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    navigation: {
+        nextEl: '.insta__swipBtn--right',
+        prevEl: '.insta__swipBtn--left',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
